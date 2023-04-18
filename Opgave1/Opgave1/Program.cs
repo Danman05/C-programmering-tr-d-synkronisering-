07ab557042ba15c2cf0ca8ef63da506ac1ab7f63
Daniel Spurrell <110387670+Danman05@users.noreply.github.com>
2023-04-18 13:06:15 +0200

    Add files via upload

---
+++ Opgave1/Opgave1/Program.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Opgave 1 - C# programmering tråd synkronisering 
+*/
+namespace Opgave1
+{
+    internal class Program
+    {
+        private static int count;
+        private static object _lock = new object();
+        static void Main(string[] args)
+        {
+
+            // ThreadPool method
+
+            ThreadPool.QueueUserWorkItem(AddTwo);
+            Thread.Sleep(500);
+            ThreadPool.QueueUserWorkItem(RemoveOne);
+
+
+            // Thread method
+
+            //Thread countUp = new Thread(AddTwo);
+            //Thread countDown = new Thread(RemoveOne);
+
+            //countUp.Start();
+            //Thread.Sleep(500);
+            //countDown.Start();
+
+            //countUp.Join();
+            //countDown.Join();
+
+            Console.Read();
+
+        }
+
+        /// <summary>
+        /// Adds two to the static count
+        /// Uses Monitor to keep the shared ressource safe
+        /// </summary>
+        /// <param name="obj"></param>
+        static void AddTwo(object obj)
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    count += 2;
+                }
+                finally
+                {
+                    Console.WriteLine($"Counter increased to: {count}");
+                    Monitor.Exit(_lock);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+
+        /// <summary>
+        /// Removes one from the static count.
+        /// Uses Monitor to keep the shared ressource safe
+        /// </summary>
+        /// <param name="obj"></param>
+        static void RemoveOne(object obj)
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    count -= 1;
+                }
+                finally
+                {
+                    Console.WriteLine($"Counter decreased to: {count}");
+                    Monitor.Exit(_lock);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Opgave2/Opgave2/Program.cs
@@ -0,0 +1,86 @@
+﻿/*
+ *  Opgave 2 + 3
+*/
+
+using System.Collections.Concurrent;
+
+namespace Opgave2
+{
+    internal class Program
+    {
+
+        // Static lock object to use in Monitor
+        // Static count to keep track of printed characters
+        static object _lock = new object();
+        static int count;
+        static void Main(string[] args)
+        {
+
+            // Threads to print out stars and hashes
+            Thread starThread = new Thread(WriteStars);
+            Thread hashThread = new Thread(WriteHashes);
+
+            starThread.Start();
+            Thread.Sleep(250);
+            hashThread.Start();
+
+            starThread.Join();
+            hashThread.Join();
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Writes 60 stars to the console, to use thread safety i use Monitor.Enter and Monitor.Exit
+        /// </summary>
+        static void WriteStars()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+
+                        Console.Write("*");
+                        count++;
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+                Console.Write($" {count}");
+                Console.WriteLine();
+                Thread.Sleep(1000);
+            }
+        }
+        /// <summary>
+        /// Writes 60 hashes to the console, to use thread safety i use Monitor.Enter and Monitor.Exit
+        /// </summary>
+        static void WriteHashes()
+        {
+            while (true)
+            {
+                Monitor.Enter(_lock);
+                try
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+
+                        Console.Write("#");
+                        count++;
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+                Console.Write($" {count}");
+                Console.WriteLine();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
